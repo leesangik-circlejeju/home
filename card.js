8ec66bf1d35ac0fe1bcc28019ae6a17ffb097848
leesangik-circlejeju <youngjininet@gmail.com>
2026-05-07 10:48:14 +0900

    Add files via upload

--- card.js
+++ card.js
@@ -5,30 +5,77 @@ const CARD_DATA = {
             category: "Announcement",
             categoryClass: "text-tertiary",
             hoverClass: "group-hover:text-tertiary",
-            title: "동그라미교육원 시설 확충 및 쾌적한 리뉴얼 안내",
-            summary: "아이들에게 더욱 나은 환경을 제공하기 위해 최신 교구 도입과 함께 내부 인테리어를 새롭게 단장했습니다.",
+            title: "동그라미교육원 프로그램 안내 블로그",
+            summary: "센타의 프로그램을 자세하게 안내하는 블로그 글입니다. 주요 프로그램등 자세한 설명이 들어있습니다.",
             date: "2024.05.01",
-            image: "https://lh3.googleusercontent.com/aida-public/AB6AXuCwMDwhwOYyebeqbj92KRmUYy54hmyURD6qHxMFTARHA7EfsxXhL8WVAKrEGQVLlHT9M5lPQ4Flmq6YFAtD1Fi-4LZNs_4RiqFye5ZmClC3bUVXfPDvrIIY1VIqqOSDGR1NBILRtUDtH5UzA-kkjaQctGKILWIvJ2W5rZOOSbGRO6SrGhYTbbSg6Squ4qrGig65KWbwY3wk0GlTiCWE73X_fCNPnYt5UZVxSOK43i8a3pXnikTQ8KVXqhNgJB3k23MyQaboURDNCKc"
+            link: "https://blog.naver.com/youngjininet/223311547560",
+            image: "cardimage/1.png"
         },
         {
             id: 2,
             category: "Technology",
             categoryClass: "text-secondary",
             hoverClass: "group-hover:text-secondary",
-            title: "첨단 인지 재활 소프트웨어 신규 버전 도입",
-            summary: "더욱 정밀한 데이터 분석이 가능한 최신 인지 훈련 엔진을 도입하여 개별 맞춤형 재활의 질을 높였습니다.",
+            title: "난독증 개선을 위한 프로그램 안내",
+            summary: "난독의 원인을 시지각, 언어기능, 뇌기능의 3대 과제 해결로 접근하여, 기존 언어치료에서 음운인식, 유창성 개선은 난독 중에 문해력에 가까운 치료이며 그 외에 재활해야 되는 요소들을 정리했습니다.",
             date: "2024.04.15",
-            image: "https://lh3.googleusercontent.com/aida-public/AB6AXuCn3O7C4E4NYDicsXi7x9LEgKoNFWo4Bs4C7jzMiyESLPXRdBr3rkHDNL0kIxP1Y0cniyFOz-XARHLI30WMru4WIG7ma236YKRG7p4tNY7Bem7q2c2thsPcIfwaR6Bol6nPg-VmzJ5V628ExANXhWNZJnRtcLWY86JZbIV4B2BnZ1sFf8ASNJDY3ynuqEtKCSm0C-lQUec7yshIDTZQMppus6hnFd32Lser4-4tYyMtcX2YmsWHJZJqi1CTkA3DMZkzrCgtKIgBDDY"
+            link: "https://blog.naver.com/youngjininet/221420182886",
+            image: "cardimage/2.png"
         },
         {
             id: 3,
             category: "Tips",
             categoryClass: "text-blue-500",
             hoverClass: "group-hover:text-blue-500",
-            title: "가정에서 할 수 있는 주의집중력 향상 놀이법",
-            summary: "일상생활 속 간단한 소품을 활용해 우리 아이의 인지 발달을 돕는 유익한 놀이 가이드를 전해드립니다.",
+            title: "경계선 지능 개선을 위한 프로그램 안내",
+            summary: "아동의 원인을 주의력,집중력,기억력등의 기초인지의 개선의 과제로 접근하여, 기존 인지치료에서 언어이해, 지각추론, 작업기억, 처리속도등의 기초인지향상",
             date: "2024.03.28",
-            image: "https://lh3.googleusercontent.com/aida-public/AB6AXuCIJC3AakSPQNf2bCNCuS7SLRgENcYthjtoRuzYT4wwwznVzhRVTgre_Va63h6ke56aHP4yN-jzBp9NTF5cDSC9tB6jZeW9IbO_a12xOY9L2L-t1P0hoN-q4J12STvZR9rUYGnybX1skd-o6NiNKCIAA7Fq-T6upNxSdiv1Nvg6htob7AebP-JpyZaePiIWgZX51YYO-AlfdStMPx4IyoJyjuHPy2hfyVyPe6PIDoXrNmsMLyesupp0vuAYtA5mfGV0UO3WqPccnCk"
+            link: "https://blog.naver.com/youngjininet/221420175192",
+            image: "cardimage/3.png"
+        },
+        {
+            id: 4,
+            category: "Announcement",
+            categoryClass: "text-tertiary",
+            hoverClass: "group-hover:text-tertiary",
+            title: "ADHD/자폐/발달장애 프로그램 안내",
+            summary: "ADHD, 자폐, 틱 등의 증상은 발달상에 나타나는 증상들로 주로 아동기나 영유아기의 조기 발견 및 치료는 매우 중요합니다. ",
+            date: "2024.05.01",
+            link: "https://blog.naver.com/youngjininet/221419798089",
+            image: "cardimage/4.png"
+        },
+        {
+            id: 5,
+            category: "Technology",
+            categoryClass: "text-secondary",
+            hoverClass: "group-hover:text-secondary",
+            title: "언어능력 향상을 위한 프로그램 안내",
+            summary: "동그라미교육원은 왜 다른 치료기관과 비교해서 다른 치료법을 이야기하는가에 대한 질문을 많이 받아왔습니다. 그것은 기존 언어치료의 개념에 언어심리학이라는 분야을 접목한 치료기법을 사용하기 때문입니다",
+            date: "2024.04.15",
+            link: "https://blog.naver.com/youngjininet/221419796675",
+            image: "cardimage/5.png"
+        },
+        {
+            id: 6,
+            category: "Tips",
+            categoryClass: "text-blue-500",
+            hoverClass: "group-hover:text-blue-500",
+            title: "첨단인지 재활 훈련 프로그램 안내",
+            summary: "첨단인지 재활에서는 CBT(Computer Based Training)기법을 활용하여 실시간 피드백, 뇌파동기화, cause&effect 효과를 이용하여 아이들의 기억력을 최대로 향상시키는 재활훈련입니다.",
+            date: "2024.03.28",
+            link: "https://blog.naver.com/youngjininet/221419796335",
+            image: "cardimage/6.png"
+        },
+        {
+            id: 7,
+            category: "Tips",
+            categoryClass: "text-blue-500",
+            hoverClass: "group-hover:text-blue-500",
+            title: "첨단인지 검사 프로그램 안내",
+            summary: "브레인 메모리 검사 (Brain Memory Test),  K-MVPT 한국형 시지각검사도구 안내",
+            date: "2024.03.28",
+            link: "https://blog.naver.com/youngjininet/221419622955",
+            image: "cardimage/7.png"
         }
     ]
 };
