ffccb97d8d916bb1e0fc9c0e91a0b055f9543c92
leesangik-circlejeju <youngjininet@gmail.com>
2026-05-07 11:02:00 +0900

    Add files via upload

--- service.js
+++ service.js
@@ -24,7 +24,7 @@ const vouchersData = [
         title: '성인 심리재활 지원 서비스',
         subtitle: '지역사회서비스',
         tag: null,
-        price: '월 18만원',
+        price: '월 20만원',
         target: '만 19세 이상 성인',
         documents: '위기가구 통합돌봄 대상자, 기초생활수급자, 의사진단및 소견서',
         service: '성인 대상 언어, 인지, 음악, 미술 프로그램 중 택 1<br>기관방문 월 4회(주1~2회) 재가방문 월 4회(주1회) 회당 50분 '
@@ -34,7 +34,7 @@ const vouchersData = [
         title: '희망나눔카드',
         subtitle: '교육청 연계 치료지원',
         tag: '추천',
-        price: '교육청 산정 금액',
+        price: '16만원',
         target: '교육청 선정 특수교육대상자',
         documents: '교육지원청 선정 통지서',
         service: '특수교육대상자의 교육권 보장 및 발달 지원을 위한 맞춤형 치료 지원 서비스'
@@ -44,7 +44,7 @@ const vouchersData = [
         title: '발달재활 서비스',
         subtitle: '보건복지부',
         tag: null,
-        price: '월 22만원',
+        price: '월 25만원',
         target: '만 18세 미만 장애아동',
         documents: '장애인 등록증, 소득 증명 서류',
         service: '장애아동의 기능 향상과 행동 발달을 위한 전문적인 재활 치료 서비스'
@@ -54,7 +54,7 @@ const vouchersData = [
         title: '장애유아치료카드',
         subtitle: '교육청 연계 치료지원',
         tag: '추천',
-        price: '교육청 산정 금액',
+        price: '16만원',
         target: '교육청 선정 특수교육대상자',
         documents: '교육지원청 선정 통지서',
         service: '특수교육대상자의 교육권 보장 및 발달 지원을 위한 맞춤형 치료 지원 서비스'
@@ -64,7 +64,7 @@ const vouchersData = [
         title: '난독/난산 서비스',
         subtitle: '기초학력 지원센터',
         tag: null,
-        price: '전액 지원',
+        price: '연간 150만원',
         target: '난독/난산 판정 초·중학생',
         documents: '기초학력 지원센터 의뢰서',
         service: '읽기 및 수 연산에 어려움이 있는 학생들을 위한 맞춤형 기초학력 향상 프로그램'
@@ -74,7 +74,7 @@ const vouchersData = [
         title: '경계선 지능 치료비 지원',
         subtitle: '기초학력 지원센터',
         tag: null,
-        price: '전액 지원',
+        price: '연간 150만원',
         target: '경계선지능 판정 초·중학생',
         documents: '기초학력 지원센터 의뢰서',
         service: '인지 및 사회성 향상 프로그램'
